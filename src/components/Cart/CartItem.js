c40119b2e1b1b9a412dfef82aafa263ab5477bf7
Rodrigo Mendoza <RODRIGO_MENDOZA@homedepot.com>
2023-08-15 10:01:30 -0600

    initial commit

---
+++ src/components/Cart/CartItem.js
@@ -0,0 +1,38 @@
+import { useDispatch } from 'react-redux';
+import classes from './CartItem.module.css';
+import { cartAction } from '../store/cart-slice';
+
+const CartItem = (props) => {
+  const dispatch = useDispatch();
+  const { id, title, quantity, total, price } = props.item;
+
+  const removeItemHandler = () => {
+    dispatch(cartAction.removeItemFromCart(id));
+  };
+  const addItemHandler = () => {
+    dispatch(cartAction.addItemToCart({ id, price, title }));
+  };
+
+  return (
+    <li className={classes.item}>
+      <header>
+        <h3>{title}</h3>
+        <div className={classes.price}>
+          ${total.toFixed(2)}{' '}
+          <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
+        </div>
+      </header>
+      <div className={classes.details}>
+        <div className={classes.quantity}>
+          x <span>{quantity}</span>
+        </div>
+        <div className={classes.actions}>
+          <button onClick={removeItemHandler}>-</button>
+          <button onClick={addItemHandler}>+</button>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default CartItem;
